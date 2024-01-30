463d8b263064f06aa0bb27b1a74f00910664445d
LilacShark <markertosummer@gmail.com>
2024-01-31 02:53:04 +0700

    Сервис эмоциональной поддержки Накидал каркас. Обновил гитигнор

---
+++ src/main/java/mts/services/CheeringInMemRepository.java
@@ -0,0 +1,7 @@
+package mts.services;
+
+/**
+ * Хранилище фраз поддержки в памяти приложения
+ */
+public class CheeringInMemRepository {
+}
---
+++ src/main/java/mts/services/CheeringManager.java
@@ -0,0 +1,4 @@
+package mts.services;
+
+public class CheeringManager {
+}
